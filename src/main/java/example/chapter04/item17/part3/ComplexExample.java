5a656c94f3b69353f0a721b14a46517b52b03fa8
YouYeol <vflag21c@nate.com>
2024-12-15 11:43:06 +0900

    Effective Java - 불변 클래스 생성 시 고려

---
+++ src/main/java/example/chapter04/item17/part3/ComplexExample.java
@@ -0,0 +1,8 @@
+package example.chapter04.item17.part3;
+
+public class ComplexExample {
+
+    public static void main(String[] args) {
+        Complex complex = Complex.valueOf(1, 0.222); //정적 팩터리 사용.
+    }
+}
