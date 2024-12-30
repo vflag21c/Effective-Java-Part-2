c94d17567f67d8a8dd5b0a1d5994fac8dfe65789
유열 <vflag21c@nate.com>
2024-12-30 22:25:47 +0900

    Effective Java - 인터페이스 & 상수

---
+++ src/main/java/example/chapter04/item22/constantinterface/MyClass.java
@@ -0,0 +1,9 @@
+package example.chapter04.item22.constantinterface;
+
+public class MyClass implements PhysicalConstants {
+
+    public static void main(String[] args) {
+        System.out.println(BOLTZMANN_CONSTANT);
+    }
+
+}
