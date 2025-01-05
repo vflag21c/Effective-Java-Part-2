7f86c2a0f9034c5b9994de0ab9c435d53a83ba52
유열 <vflag21c@nate.com>
2025-01-05 22:09:58 +0900

    Effective Java - 비검사 경고

---
+++ src/main/java/example/chaptor05/item27/unchecked/SetExample.java
@@ -0,0 +1,13 @@
+package example.chaptor05.item27.unchecked;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+
+    public static void main(String[] args) {
+        Set names = new HashSet(); //로 타입.
+
+        Set<String> strings = new HashSet<>(); //다이아몬드 연산자
+    }
+}
