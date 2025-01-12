a7f82b9191c1c3d98f22c2fdbdd1787f8008b169
YouYeol <vflag21c@nate.com>
2025-01-12 12:00:43 +0900

    Effective Java - 가변인자 & 제네릭

---
+++ src/main/java/example/chaptor05/item28/safevarags/SafeVaragsExample.java
@@ -0,0 +1,27 @@
+package example.chaptor05.item28.safevarags;
+
+import java.util.List;
+
+public class SafeVaragsExample {
+
+//    @SafeVarargs // Not actually safe!
+    static void notSafe(List<String>... stringLists) {
+        Object[] array = stringLists; // List<String>... => List[], 그리고 배열은 공변이니까.
+        List<Integer> tmpList = List.of(42);
+        array[0] = tmpList;
+        String s = stringLists[0].get(0); //ClassCastException at runtime
+    }
+
+    @SafeVarargs
+    static <T> void safe(T... values) {
+        for (T value: values) {
+            System.out.println(value);
+        }
+    }
+
+    public static void main(String[] args) {
+        SafeVaragsExample.safe("a", "b", "c");
+        SafeVaragsExample.notSafe(List.of("a", "b", "c"));
+    }
+
+}
---
+++ src/main/java/example/chaptor05/item29/EmptyStackException.java
@@ -0,0 +1,4 @@
+package example.chaptor05.item29;
+
+public class EmptyStackException extends RuntimeException {
+}
